c85495570d22200846fc688d18358a946318cd06
Sladja <sladjamatic92@gmail.com>
2022-08-19 00:34:27 -0400

    updating Login.java X2

--- src/day2/Login.java
+++ src/day2/Login.java
@@ -3,5 +3,6 @@ package day2;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Add you username and password!");
+        System.out.println("What is your name?");
     }
 }
